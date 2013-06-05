e2b543a49722921eb511ad7266c1e2cc15b0dde3
Chong-Yee Khoo <mail@cykhoo.com>
2013-06-06 00:41:04 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freemyinternet::Application.config.secret_key_base = '9bf3ed413b4ebf305601d585cecdefe074d72cfa038c0760fbfdaf54805db96baf575004f6762518ec1e0fa50c572f032868ef046ab7ff46a326a175e85193fc'
